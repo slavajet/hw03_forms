0378af9ab9f2b2bfdc8ae3b3a6dff93dec78d454
Герасимович Вячеслав <slavacaas@yandex.ru>
2023-03-07 20:27:43 +0600

    flake8

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -29,32 +29,44 @@ urlpatterns = [
     ),
     path(
         'password_change/',
-        PasswordChangeView.as_view(template_name='users/password_change_form.html'),
+        PasswordChangeView.as_view(
+            template_name='users/password_change_form.html'
+        ),
         name='password_change'
     ),
     path(
         'password_change/done/',
-        PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),
+        PasswordChangeDoneView.as_view(
+            template_name='users/password_change_done.html'
+        ),
         name='password_change_done'
     ),
     path(
         'password_reset/',
-        PasswordResetView.as_view(template_name='users/password_reset_form.html'),
+        PasswordResetView.as_view(
+            template_name='users/password_reset_form.html'
+        ),
         name='password_reset'
     ),
     path(
         'password_reset/done/',
-        PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+        PasswordResetDoneView.as_view(
+            template_name='users/password_reset_done.html'
+        ),
         name='password_reset_done'
     ),
     path(
         'reset/<uidb64>/<token>/',
-        PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+        PasswordResetConfirmView.as_view(
+            template_name='users/password_reset_confirm.html'
+        ),
         name='password_reset_confirm'
     ),
     path(
         'reset/done/',
-        PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+        PasswordResetCompleteView.as_view(
+            template_name='users/password_reset_complete.html'
+        ),
         name='password_reset_complete'
     )
 ]
